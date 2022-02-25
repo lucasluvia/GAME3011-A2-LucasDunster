0795dd66e9778aeced4beb0fcc11669ebe97ca81
lucasdunster <60508486+lucasluvia@users.noreply.github.com>
2022-02-24 23:37:34 -0500

    Initial commit

---
+++ Assets/[Scripts]/GameController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    // Variables
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    //public void AddToPointTracker(int pointsToAdd)
+    //{
+    //    pointTracker += pointsToAdd;
+    //    pointsText.text = pointTracker.ToString();
+    //}
+
+}
